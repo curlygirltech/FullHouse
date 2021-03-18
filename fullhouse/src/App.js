cd7b6c2760afa8241c81c2d79eb4e05e7a1db413
jcameron10 <jovonnec@gmail.com>
2021-03-18 17:54:05 -0400

    footer

--- fullhouse/src/App.js
+++ fullhouse/src/App.js
@@ -1,4 +1,3 @@
-// import Fullhouse from "./Fullhouse.png"
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import SignUpForm from "./components/SignUpForm";
--- fullhouse/src/components/Footer.jsx
+++ fullhouse/src/components/Footer.jsx
@@ -4,8 +4,11 @@ export default function Footer() {
   return (
     <div className="footer">
       <p className="footer-name">Jovonne Cameron</p>
+      <div className="social-media">
+
       <a className="linked-in" href="https://www.linkedin.com/in/jovonnecameron/">LinkedIn </a>
       <a className="github" href="https://github.com/curlygirltech">GitHub</a>
+      </div>
   
     
     </div>
