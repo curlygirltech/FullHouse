0dfaa9f5b8b52fed31c12ea0e04867b8515c561b
jcameron10 <jovonnec@gmail.com>
2021-03-19 20:16:27 -0400

    css connection page

--- fullhouse/src/components/ConnectionPage.jsx
+++ fullhouse/src/components/ConnectionPage.jsx
@@ -1,21 +1,18 @@
 import axios from "axios";
-import "../componentcss/connection.css"
-import { baseURL, config } from "../services"
-
-
+import "../componentcss/connection.css";
+import { baseURL, config } from "../services";
 
 export default function ConnectionPage(props) {
-  const { data, role,} = props;
-  
-  
+  const { data, role } = props;
+
   const deleteMember = async (memberId) => {
-    console.log(memberId)
-    
-    const memberURL = `${baseURL}/${memberId}`
-    await axios.delete(memberURL, config)
-    props.setToggleFetch((curr)=>!curr)
-  }
-    
+    console.log(memberId);
+
+    const memberURL = `${baseURL}/${memberId}`;
+    await axios.delete(memberURL, config);
+    props.setToggleFetch((curr) => !curr);
+  };
+
   const filtered = data.filter((member) => {
     return member.fields.relationship !== role;
   });
@@ -24,10 +21,11 @@ export default function ConnectionPage(props) {
   //use filter method through entries based on whats in props, get the mentees or mentors
   //output that on the page
   return (
-    <div className="connect-container">
+    <div>
+      <div className="connect-container">
         <h3>Let's Connect</h3>
         {filtered.map((person) => {
-          console.log(person)
+          console.log(person);
           const {
             name,
             contact,
@@ -36,13 +34,18 @@ export default function ConnectionPage(props) {
             languages,
           } = person.fields;
           return (
-            <div >
+            <div>
               <h1>{name}</h1>
               <p>{languages}</p>
               <p>{aboutMe}</p>
               <p>{relationship}</p>
               <p>{contact}</p>
-              <button className="delete-button" onClick={()=>deleteMember(person.id)}>Delete</button>
+              <button
+                className="delete-button"
+                onClick={() => deleteMember(person.id)}
+              >
+                Delete
+              </button>
               {/* //calling the function with an argument. i need to invoke the function inside of an anonoymous function */}
             </div>
           );
